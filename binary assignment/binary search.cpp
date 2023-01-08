423b2317caeb5c49fb36077bb4cde7fc79ef1004
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2023-01-08 19:24:26 +0530

    Create binary search.cpp

---
+++ binary assignment/binary search.cpp
@@ -0,0 +1,21 @@
+ int search(vector<int>& nums, int target) 
+    {
+        int l=0,h=nums.size();
+        while(l<h)
+        {
+            int mid=(l+h)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                h=mid;
+            }
+        }
+        return -1; 
+    }
