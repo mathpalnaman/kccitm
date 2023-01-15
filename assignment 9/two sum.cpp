b6463bad25b5b19bc6ee1fe45b95b0a8437d908d
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2023-01-15 23:05:47 +0530

    Create two sum.cpp

---
+++ assignment 9/two sum.cpp
@@ -0,0 +1,9 @@
+ unordered_map<int, int> numToIndex;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (numToIndex.count(target - nums[i]))
+        return {numToIndex[target - nums[i]], i};
+      numToIndex[nums[i]] = i;
+    }
+
+    throw;
